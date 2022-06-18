9b26891d8847b44f80e0c3dcf5d37785e877b454
Andre <andreraizdetudo@gmail.com>
2022-06-18 09:22:54 -0300

    ajustando commits

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+const botaoMenu = document.querySelector('.cabecalho__menu');
+const menuLateral = document.querySelector('.menu-lateral');
+botaoMenu.addEventListener('click', () => {	
+    menuLateral.classList.toggle('menu-lateral--ativo');
+});
